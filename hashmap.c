c25e7d0cf0b59060f4afe7e4d2c3a4b85f8ddefa
RamiroMagnater1 <>
2022-04-10 23:51:16 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -82,17 +82,16 @@ Pair * searchMap(HashMap * map,  char * key) {
 
   int pos = hash(key, map->capacity);
   
-  if(map->buckets[pos]->key == key){
-    return map->buckets[pos];
+  if(map->buckets[pos]->key != key){
+    while(map->buckets[pos]->key != key){
+      pos++;
+      if(map->buckets[pos] == NULL) return NULL;
+    }
   }
-  /*
-  while(map->buckets[pos]->key != key){
-    pos++;
-    if(map->buckets[pos] == NULL) return NULL;
-  }
-  */
+  
   map->current = pos;
-
+  return map->buckets[pos];
+}
 
 Pair * firstMap(HashMap * map) {
 
