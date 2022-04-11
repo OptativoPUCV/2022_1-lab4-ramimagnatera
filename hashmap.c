40706605455b3df343c0c0512fb45cf9de110eaa
RamiroMagnater1 <>
2022-04-11 00:45:44 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -101,7 +101,7 @@ Pair * searchMap(HashMap * map,  char * key) {
 Pair * firstMap(HashMap * map) {
   int pos = 0;
 
-  while(map->buckets[pos] == NULL){
+  while(map->buckets[pos] == NULL || map->buckets[pos]->key == NULL){
     pos++;
   }
   map->current = pos;
