963d43f4dc284cb8176df660b2421b4405422627
RamiroMagnater1 <>
2022-04-11 01:44:40 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -113,7 +113,7 @@ Pair * firstMap(HashMap * map) {
 Pair * nextMap(HashMap * map) {
   int pos = map->current;
 
-  if(map->current+1 == map->capacity){
+  if(map->current+1 == map->capacity || map->current == 0){
     return NULL;
   }
   
