93c7dcb3100b89b2f1f53a358d44a666898eaac8
RamiroMagnater1 <>
2022-04-11 00:57:24 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -109,6 +109,11 @@ Pair * firstMap(HashMap * map) {
 }
 
 Pair * nextMap(HashMap * map) {
+  int pos = map->current;
 
-  return NULL;
+  while(map->buckets[pos] == NULL || map->buckets[pos]->key == NULL){
+    pos++;
+  }
+  map->current = pos;
+  return map->buckets[pos];
 }
